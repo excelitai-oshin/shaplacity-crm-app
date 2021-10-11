dcfcf915644fe4dffdf64418d3d20d131a2b6d6c
excelitai-oshin <oshin@excelitai.com>
2021-10-11 15:08:36 +0600

    first commit

---
+++ app/src/main/java/com/example/shaplacity/ApplicantInfo2.java
@@ -0,0 +1,14 @@
+package com.example.shaplacity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ApplicantInfo2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_applicant_info2);
+    }
+}
---
+++ app/src/main/java/com/example/shaplacity/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.shaplacity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        TextView username =(TextView) findViewById(R.id.username);
+        TextView password =(TextView) findViewById(R.id.Password);
+
+        Button loginbtn =(Button) findViewById(R.id.loginbtn);
+                // admin
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            //profile
+            public void onClick(View v) {
+               startActivity(new Intent(MainActivity .this,profile.class));
+            }
+        });
+    }
+}
